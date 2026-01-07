7b3c227f05fcf43fe9a8a35b8b787f8621721dfc
Ashir Rao <69091220+Ash1R@users.noreply.github.com>
2026-01-07 14:46:38 -0800

    new tests

--- include/RuntimeValue.h
+++ include/RuntimeValue.h
@@ -18,21 +18,6 @@ using RuntimeValue = std::variant<std::monostate, double, bool, std::string, std
 
 std::string to_string(const RuntimeValue& value);
 
-// We need to verify if Callable definition is available for to_string or handled separately.
-// Since we only hold shared_ptr, it's fine. But to_string impl needs Callable definition if we use it here.
-// We can move to_string impl to a cpp file or header that includes Callable.h, but circular include!
-// Better to make to_string a declaration here and impl in Interpreter.cpp or RuntimeValue.cpp?
-// For now, let's keep to_string inline but check types carefully.
-// If we include Callable.h here, we get circular dep if Callable includes RuntimeValue.
-// Callable.h includes RuntimeValue.h. So RuntimeValue.h CANNOT include Callable.h.
-// So we must forward declare Callable.
-// But we cannot call toString() on undefined type.
-// So we must move to_string implementation out of inline header OR include Callable.h inside the function or similar hacks.
-// Best practice: Move to_string logic to a .cpp file or make it a separate utility. 
-// OR: Include Callable.h IN THE CPP files that use RuntimeValue, and here just use forward decl. 
-// But to_string is used in Interpreter.cpp. 
-// Let's remove inline definition of to_string from header and declare it.
-
 std::string to_string(const RuntimeValue& value);
 
 
--- include/TokenType.h
+++ include/TokenType.h
@@ -2,27 +2,22 @@
 #define TOKENTYPE_H
 
 enum class TokenType {
-    // Single-character tokens
     LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
     LEFT_BRACKET, RIGHT_BRACKET,
     COMMA, SEMICOLON, MINUS, PLUS, SLASH, STAR, PERCENT, AT, DOT,
 
-    // One or two character tokens
     BANG, BANG_EQUAL,
     EQUAL, EQUAL_EQUAL,
     GREATER, GREATER_EQUAL,
     LESS, LESS_EQUAL,
     AMPERSAND_AMPERSAND, PIPE_PIPE,
 
-    // Literals
     IDENTIFIER, STRING, NUMBER,
 
-    // Keywords
     FN, LET, IF, ELSE, WHILE, RETURN, PRINT, MODEL,
     TRUE, FALSE,
 
-    // End of file
     END_OF_FILE
 };
 
-#endif // TOKENTYPE_H
+#endif 
